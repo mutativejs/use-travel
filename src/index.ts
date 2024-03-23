f82140caa9d1a121f92f4245785719056b496ecb
Michael Lin <unadlib@gmail.com>
2024-03-23 13:24:12 +0800

    feat(archive): implement archive API (#1)

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,24 @@ type TravelPatches = {
   inversePatches: Patches[];
 };
 
-type Options<F extends boolean> = {
+type Options<F extends boolean, A extends boolean> = {
+  /**
+   * The maximum number of history to keep, by default `10`
+   */
   maxHistory?: number;
+  /**
+   * The initial position in the history, by default `0`
+   */
+  initialPosition?: number;
+  /**
+   * The initial patches of the history
+   */
   initialPatches?: TravelPatches;
-} & MutativeOptions<true, F>;
+  /**
+   * Whether to automatically archive the current state, by default `true`
+   */
+  autoArchive?: A;
+} & Omit<MutativeOptions<true, F>, 'enablePatches'>;
 
 type InitialValue<I extends any> = I extends (...args: any) => infer R ? R : I;
 type DraftFunction<S> = (draft: Draft<S>) => void;
@@ -25,62 +39,81 @@ type Updater<S> = (value: S | (() => S) | DraftFunction<S>) => void;
 type Value<S, F extends boolean> = F extends true
   ? Immutable<InitialValue<S>>
   : InitialValue<S>;
-type StateValue<S> =
-  | InitialValue<S>
-  | (() => InitialValue<S>)
-  | DraftFunction<InitialValue<S>>;
 
-type Result<S, F extends boolean> = [
+interface Controls<S, F extends boolean> {
+  /**
+   * The current position in the history
+   */
+  position: number;
+  /**
+   * Get the history of the state
+   */
+  getHistory: () => Value<S, F>[];
+  /**
+   * The patches of the history
+   */
+  patches: TravelPatches;
+  /**
+   * Go back in the history
+   */
+  back: (amount?: number) => void;
+  /**
+   * Go forward in the history
+   */
+  forward: (amount?: number) => void;
+  /**
+   * Reset the history
+   */
+  reset: () => void;
+  /**
+   * Go to a specific position in the history
+   */
+  go: (position: number) => void;
+  /**
+   * Check if it's possible to go back
+   */
+  canBack: () => boolean;
+  /**
+   * Check if it's possible to go forward
+   */
+  canForward: () => boolean;
+}
+
+type Result<S, F extends boolean, A extends boolean> = [
   Value<S, F>,
   Updater<InitialValue<S>>,
-  {
-    /**
-     * The current position in the history
-     */
-    position: number;
-    /**
-     * Get the history of the state
-     */
-    getHistory: () => Value<S, F>[];
-    /**
-     * The patches of the history
-     */
-    patches: TravelPatches;
-    /**
-     * Go back in the history
-     */
-    back: (amount?: number) => void;
-    /**
-     * Go forward in the history
-     */
-    forward: (amount?: number) => void;
-    /**
-     * Reset the history
-     */
-    reset: () => void;
-    /**
-     * Go to a specific position in the history
-     */
-    go: (position: number) => void;
-    /**
-     * Check if it's possible to go back
-     */
-    canBack: () => boolean;
-    /**
-     * Check if it's possible to go forward
-     */
-    canForward: () => boolean;
-  }
+  A extends false
+    ? Controls<S, F> & {
+        /**
+         * Archive the current state
+         */
+        archive: () => void;
+      }
+    : Controls<S, F>
 ];
 
 /**
  * A hook to travel in the history of a state
  */
-export const useTravel = <S, F extends boolean>(
+export const useTravel = <S, F extends boolean, A extends boolean>(
   initialState: S,
-  { maxHistory = 10, initialPatches, ...options }: Options<F> = {}
+  _options: Options<F, A> = {}
 ) => {
-  const [position, setPosition] = useState(0);
+  const {
+    maxHistory = 10,
+    initialPatches,
+    initialPosition = 0,
+    autoArchive = true,
+    ...options
+  } = _options;
+  const [position, setPosition] = useState(initialPosition);
+  const [tempPatches, setTempPatches] = useMutative(
+    () =>
+      ({
+        patches: [],
+        inversePatches: [],
+      } as TravelPatches)
+  );
   const [allPatches, setAllPatches] = useMutative(
     () =>
       (initialPatches ?? {
@@ -97,38 +130,113 @@ export const useTravel = <S, F extends boolean>(
           : create(state, () => updater, { ...options, enablePatches: true })
       ) as [S, Patches<true>, Patches<true>];
       setState(nextState);
-      setPosition(position + 1);
-      setAllPatches((_allPatches) => {
-        const notLast = position < _allPatches.patches.length;
+      if (autoArchive) {
+        setPosition(
+          maxHistory < allPatches.patches.length + 1 ? maxHistory : position + 1
+        );
+        setAllPatches((allPatchesDraft) => {
+          const notLast = position < allPatchesDraft.patches.length;
+          // Remove all patches after the current position
+          if (notLast) {
+            allPatchesDraft.patches.splice(
+              position,
+              allPatchesDraft.patches.length - position
+            );
+            allPatchesDraft.inversePatches.splice(
+              position,
+              allPatchesDraft.inversePatches.length - position
+            );
+          }
+          allPatchesDraft.patches.push(patches);
+          allPatchesDraft.inversePatches.push(inversePatches);
+          if (maxHistory < allPatchesDraft.patches.length) {
+            allPatchesDraft.patches = allPatchesDraft.patches.slice(
+              -maxHistory
+            );
+            allPatchesDraft.inversePatches =
+              allPatchesDraft.inversePatches.slice(-maxHistory);
+          }
+        });
+      } else {
+        const notLast =
+          position <
+          allPatches.patches.length + Number(!!tempPatches.patches.length);
         // Remove all patches after the current position
         if (notLast) {
-          _allPatches.patches.splice(
+          allPatches.patches.splice(
             position,
-            _allPatches.patches.length - position
+            allPatches.patches.length - position
           );
-          _allPatches.inversePatches.splice(
+          allPatches.inversePatches.splice(
             position,
-            _allPatches.inversePatches.length - position
+            allPatches.inversePatches.length - position
           );
         }
-        _allPatches.patches.push(patches);
-        _allPatches.inversePatches.push(inversePatches);
-        if (maxHistory < _allPatches.patches.length) {
-          _allPatches.patches = _allPatches.patches.slice(-maxHistory);
-          _allPatches.inversePatches = _allPatches.inversePatches.slice(
-            -maxHistory
+        if (!tempPatches.patches.length || notLast) {
+          setPosition(
+            maxHistory < allPatches.patches.length + 1
+              ? maxHistory
+              : position + 1
           );
         }
-      });
+        setTempPatches((tempPatchesDraft) => {
+          if (notLast) {
+            tempPatchesDraft.patches.length = 0;
+            tempPatchesDraft.inversePatches.length = 0;
+          }
+          tempPatchesDraft.patches.push(patches);
+          tempPatchesDraft.inversePatches.push(inversePatches);
+        });
+      }
     },
-    [state, position]
+    [state, position, setTempPatches]
   );
+  const archive = () => {
+    if (autoArchive) {
+      console.warn('Auto archive is enabled, no need to archive manually');
+      return;
+    }
+    if (!tempPatches.patches.length) return;
+    setAllPatches((allPatchesDraft) => {
+      allPatchesDraft.patches.push(tempPatches.patches.flat());
+      allPatchesDraft.inversePatches.push(tempPatches.inversePatches.flat());
+      if (maxHistory < allPatchesDraft.patches.length) {
+        allPatchesDraft.patches = allPatchesDraft.patches.slice(-maxHistory);
+        allPatchesDraft.inversePatches = allPatchesDraft.inversePatches.slice(
+          -maxHistory
+        );
+      }
+    });
+    setTempPatches((tempPatchesDraft) => {
+      tempPatchesDraft.patches.length = 0;
+      tempPatchesDraft.inversePatches.length = 0;
+    });
+  };
+
+  const _allPatches = useMemo(() => {
+    const shouldArchive = !(autoArchive || !tempPatches.patches.length);
+    let mergedPatches = allPatches;
+    if (shouldArchive) {
+      mergedPatches = {
+        patches: allPatches.patches.concat(),
+        inversePatches: allPatches.inversePatches.concat(),
+      };
+      mergedPatches.patches.push(tempPatches.patches.flat());
+      mergedPatches.inversePatches.push(tempPatches.inversePatches.flat());
+    }
+    return mergedPatches;
+  }, [allPatches, tempPatches]);
+
   const cachedTravels = useMemo(() => {
     const go = (nextPosition: number) => {
       const back = nextPosition < position;
-      if (nextPosition > allPatches.patches.length) {
+      const shouldArchive = !(autoArchive || !tempPatches.patches.length);
+      if (shouldArchive) {
+        archive();
+      }
+      if (nextPosition > _allPatches.patches.length) {
         console.warn(`Can't go forward to position ${nextPosition}`);
-        nextPosition = allPatches.patches.length;
+        nextPosition = _allPatches.patches.length;
       }
       if (nextPosition < 0) {
         console.warn(`Can't go back to position ${nextPosition}`);
@@ -140,8 +248,21 @@ export const useTravel = <S, F extends boolean>(
           apply(
             state as object,
             back
-              ? allPatches.inversePatches.slice(nextPosition).flat().reverse()
-              : allPatches.patches.slice(position, nextPosition).flat()
+              ? _allPatches.inversePatches
+                  .slice(
+                    _allPatches.inversePatches.length - maxHistory,
+                    _allPatches.inversePatches.length
+                  )
+                  .slice(nextPosition)
+                  .flat()
+                  .reverse()
+              : _allPatches.patches
+                  .slice(
+                    _allPatches.inversePatches.length - maxHistory,
+                    _allPatches.inversePatches.length
+                  )
+                  .slice(position, nextPosition)
+                  .flat()
           ) as S
       );
       setPosition(nextPosition);
@@ -153,19 +274,23 @@ export const useTravel = <S, F extends boolean>(
         if (cachedHistory) return cachedHistory;
         cachedHistory = [state];
         let currentState = state as any;
-        for (let i = position; i < allPatches.patches.length; i++) {
-          currentState = apply(
-            currentState as object,
-            allPatches.patches[i]
-          ) as S;
+        const patches =
+          !autoArchive && _allPatches.patches.length > maxHistory
+            ? _allPatches.patches.slice(_allPatches.patches.length - maxHistory)
+            : _allPatches.patches;
+        const inversePatches =
+          !autoArchive && _allPatches.patches.length > maxHistory
+            ? _allPatches.inversePatches.slice(
+                _allPatches.inversePatches.length - maxHistory
+              )
+            : _allPatches.inversePatches;
+        for (let i = position; i < patches.length; i++) {
+          currentState = apply(currentState as object, patches[i]) as S;
           cachedHistory.push(currentState);
         }
         currentState = state as any;
         for (let i = position - 1; i > -1; i--) {
-          currentState = apply(
-            currentState as object,
-            allPatches.inversePatches[i]
-          ) as S;
+          currentState = apply(currentState as object, inversePatches[i]) as S;
           cachedHistory.unshift(currentState);
         }
         return cachedHistory;
@@ -178,15 +303,25 @@ export const useTravel = <S, F extends boolean>(
         go(position + amount);
       },
       reset: () => {
-        setPosition(0);
+        setPosition(initialPosition);
         setAllPatches(
           () => initialPatches ?? { patches: [], inversePatches: [] }
         );
         setState(() => initialState);
+        setTempPatches(() => ({ patches: [], inversePatches: [] }));
       },
       go,
-      canBack: () => position > 0,
-      canForward: () => position < allPatches.patches.length,
+      canBack: () => {
+        return position > 0;
+      },
+      canForward: () => {
+        const shouldArchive = !(autoArchive || !tempPatches.patches.length);
+        if (shouldArchive) {
+          return position < _allPatches.patches.length - 1;
+        }
+        return position < allPatches.patches.length;
+      },
+      archive,
     };
   }, [
     position,
@@ -196,6 +331,8 @@ export const useTravel = <S, F extends boolean>(
     setState,
     initialState,
     state,
+    tempPatches,
+    _allPatches,
   ]);
-  return [state, cachedSetState, cachedTravels] as Result<S, F>;
+  return [state, cachedSetState, cachedTravels] as Result<S, F, A>;
 };
