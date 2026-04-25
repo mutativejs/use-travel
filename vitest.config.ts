566888878cdf19dff8fa693e50c9aeeb49354e59
unadlib <unadlib@gmail.com>
2026-04-25 23:20:46 +0800

    test(config): update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -8,6 +8,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'lcov'],
       reportsDirectory: './coverage',
+      exclude: [...coverageConfigDefaults.exclude, 'test-d/**'],
     },
   },
   define: {
